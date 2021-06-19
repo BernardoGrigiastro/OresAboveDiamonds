17ba097b9f32498dfa6bcee7993a2ebf39125935
Dvitash <dvitash3414@gmail.com>
2021-06-18 20:52:48 -0400

    fixed xp not being dropped when mining ores

---
+++ src/main/java/dvi/oresabovediamondsfabric/common/main/items/ModOres.java
@@ -0,0 +1,24 @@
+package dvi.oresabovediamondsfabric.common.main.items;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.block.OreBlock;
+import net.minecraft.util.math.MathHelper;
+
+import java.util.Random;
+
+public class ModOres extends OreBlock {
+    public ModOres(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    protected int getExperienceWhenMined(Random random) {
+        if (this == ModObjects.AMETHYST_ORE) {
+            return MathHelper.nextInt(random, 4, 10);
+        } else if (this == ModObjects.BLACK_OPAL_ORE) {
+            return MathHelper.nextInt(random, 6, 20);
+        } else {
+            return 0;
+        }
+    }
+}
